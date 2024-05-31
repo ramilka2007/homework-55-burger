ef141db9770acfde9659ebe3bc1a20b64ef1e1eb
ramilka <lilya1976.03.03@gmail.com>
2024-06-01 02:28:36 +0600

    В tsconfig.json, я сделал расширение tsx файлов false; в main.tsx, я убрал расширение tsx; добавил картинки; сделал один массив ingredientList и передал его в Ingredients.tsx, где я сделал список ингредиентов; подключил необходимый css для бургера и сделал дизайн окошка с ингредиентами и бургером.

---
+++ src/containers/App.tsx
@@ -0,0 +1,54 @@
+import './App.css'
+import {Ingredient} from "../types";
+import saladImage from "../assets/saladImg.png";
+import cheeseImage from "../assets/cheeseImg.png";
+import baconImage from "../assets/baconImg.png";
+import meatImage from "../assets/meatImg.png";
+import {useState} from "react";
+import Ingredients from "../components/Ingredients/Ingredients";
+
+const App = () => {
+    const INGREDIENTS: Ingredient[] = [
+        {name: 'Salad', price: 10, image: `${saladImage}`},
+        {name: 'Cheese', price: 50, image: `${cheeseImage}`},
+        {name: 'Bacon', price: 60, image: `${baconImage}`},
+        {name: 'Meat', price: 80, image: `${meatImage}`},
+    ];
+
+    const [ingredients] = useState([
+        {name: 'Salad', count: 0},
+        {name: 'Cheese', count: 0},
+        {name: 'Bacon', count: 0},
+        {name: 'Meat', count: 0},
+    ]);
+
+    const ingredientsList = ingredients.map((ingredient, index) => ({
+        ...ingredient, ...INGREDIENTS[index]}));
+
+    return (
+        <div className="menu">
+            <div className="chooseIngredients">
+                <div className="title"><h1>Ingredients</h1></div>
+                {ingredientsList.map((ingredient) => (
+                    <Ingredients
+                        name={ingredient.name}
+                        image={ingredient.image}
+                        count={ingredient.count}
+                    />
+                ))}
+            </div>
+            <div className="burgerDiv">
+                <div className="title"><h1>Burger</h1></div>
+                <div className="Burger">
+                    <div className="BreadTop">
+                        <div className="Seeds1"></div>
+                        <div className="Seeds2"></div>
+                    </div>
+                    <div className="BreadBottom"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App
